fea06442b3cc0ac525a29fbe5d00fb4a93dd3ee9
Alex-GF <yalejandro9@gmail.com>
2024-10-16 10:08:30 +0200

    FEAT: IMPROVED TESTING STRATEGY
    I have parametrized all the parsing tests and created a first suite for the fields: saasName, version and createdAt

--- src/utils/pricing-validators.ts
+++ src/utils/pricing-validators.ts
@@ -5,8 +5,12 @@ import { LATEST_PRICING2YAML_VERSION } from "./version-manager.ts";
 const VERSION_REGEXP = /^\d+\.\d+$/;
 
 export function validateName(name: string | null, item: string): string {
-    if (name === null) {
-        throw new Error(`The ${item} name must not be null`);
+    if (name === null || name === undefined) {
+        throw new Error(`The ${item} must have a name`);
+    }
+
+    if (typeof name !== "string"){
+        throw new Error(`The ${item} name must be a string`);
     }
 
     const trimmedName = name.trim();
@@ -28,7 +32,7 @@ export function validateName(name: string | null, item: string): string {
 
 export function validateVersion(version: string): SemVer{
     if (version === null || version === undefined) {
-        throw new Error(`The version field of the pricing must not be null or undefined. Please ensure that the version field is present and correctly formatted.`);
+        throw new Error(`The version field of the pricing must not be null or undefined. Please ensure that the version field is present and correctly formatted`);
     }
 
     if (typeof version !== 'string' || !VERSION_REGEXP.test(version)) {
@@ -38,7 +42,7 @@ export function validateVersion(version: string): SemVer{
     const parsedVersion = semver.parse(version + ".0");
 
     if (!semver.equals(parsedVersion, LATEST_PRICING2YAML_VERSION)) {
-        throw new Error(`The version field of the pricing is not equal to the last supported version. Please ensure you are using exactly the version ${LATEST_PRICING2YAML_VERSION.major}.${LATEST_PRICING2YAML_VERSION.minor}`);
+        throw new Error(`The version field of the pricing does not correspond to the latest version. Please ensure you are using it`);
     }
 
     return parsedVersion;
@@ -46,7 +50,7 @@ export function validateVersion(version: string): SemVer{
 
 export function validateCreatedAt(createdAt: string | Date | null): Date{
     if (createdAt === null || createdAt === undefined) {
-        throw new Error(`The createdAt field must not be null or undefined. Please ensure that the createdAt field is present and correctly formatted (as Date or string).`);
+        throw new Error(`The createdAt field must not be null or undefined. Please ensure that the createdAt field is present and correctly formatted (as Date or string)`);
     }
 
     if (typeof createdAt === 'string') {
@@ -54,12 +58,12 @@ export function validateCreatedAt(createdAt: string | Date | null): Date{
     }
 
     if (!(createdAt instanceof Date) || isNaN(createdAt.getTime())) {
-        throw new Error(`The createdAt field must be a valid Date object or a string in a recognized date format.`);
+        throw new Error(`The createdAt field must be a valid Date object or a string in a recognized date format`);
     }
 
     const now = new Date();
     if (createdAt > now) {
-        throw new Error(`The createdAt field must not be a future date.`);
+        throw new Error(`The createdAt field must not be a future date`);
     }
 
     return createdAt;
--- tests/yaml/parser.test.ts
+++ tests/yaml/parser.test.ts
@@ -8,43 +8,132 @@
  * The tests perform the following actions:
  * - Before each test, a temporary file is created from a predefined YAML file.
  * - After each test, the temporary file is removed to ensure a clean state.
- * - The main tests verify multiple aspects of the YAML parsing functionality.
+ * - Positive tests verify that the YAML parsing correctly retrieves the expected pricing information.
+ * - Negative tests verify that the YAML parsing throws the expected errors for invalid inputs.
  * 
  * @module tests/yaml/parser.test
  * @requires jsr:@std/testing/bdd
  * @requires ../../src/utils/yaml-utils.ts
- * @requires ../../src/models/pricing.d.ts
+ * @requires ../../src/models/pricing.ts
  * @requires @std/assert
+ * @requires ../../src/utils/version-manager.ts
+ * @requires @std/csv
  */
 
-import { afterEach, beforeEach, describe, it } from "jsr:@std/testing/bdd";
+import { after, before, describe, it } from "jsr:@std/testing/bdd";
 import { retrievePricingFromYaml } from "../../src/utils/yaml-utils.ts";
 import { Pricing } from "../../src/models/pricing.ts";
-import { assert, assertEquals } from "@std/assert";
+import { assert, assertEquals, assertIsError } from "@std/assert";
 import { LATEST_PRICING2YAML_VERSION } from "../../src/utils/version-manager.ts";
+import * as csv from "@std/csv";
 
-const TEST_PRICING_YAML_PATH = "tests/resources/pricing/petclinic.yml";
-const TEMP_FILE_PATH = "tests/resources/temp/test_pricing.yml";
-const TEST_PRICING_NAME = "PetClinic";
+const POSITIVE_TESTS_CSV_PATH = "tests/yaml/data/positive-parsing-tests.csv";
+const NEGATIVE_TESTS_CSV_PATH = "tests/yaml/data/negative-parsing-tests.csv";
+const TEMP_FILE_PATH = "tests/resources/temp/test_";
 
-beforeEach(() => {
-    // Create a temp file from the TEST_PRICING_YAML_PATH file
-    Deno.copyFileSync(TEST_PRICING_YAML_PATH, TEMP_FILE_PATH);
-});
+interface Test{
+    testName: string,
+    pricingPath: string,
+    expected: string
+}
 
-afterEach(() => {
-    // Remove the modified temp file
-    Deno.removeSync(TEMP_FILE_PATH);
-});
+interface TestSection{
+    sectionName: string,
+    tests: Test[]
+}
 
-describe("Yaml Parser Tests", () => {
-    it("Pricing parsed correctly", () => {
-        const pricing: Pricing = retrievePricingFromYaml(TEMP_FILE_PATH);
+function readCSVFile(filePath: string): string[][] {
+    
+    const absolutePath: string = Deno.realPathSync(filePath);
+    const csvContent = Deno.readTextFileSync(absolutePath);
+    const content: string[][] = csv.parse(csvContent.split('\n').slice(1).join('\n'));
+
+    return content;
+
+}
+
+function parseCSVContent(content: string[][]): TestSection[] {
+    const result: TestSection[] = [];
+    let i = -1; // In order to match the index 0 once the first test section is added
+    for (const entry of content) {
+        if (entry[1] === "-") {
+            result.push({
+                sectionName: entry[0],
+                tests: []
+            });
+            i++;
+            continue;
+        }
+
+        result[i].tests.push({
+            testName: entry[0],
+            pricingPath: entry[1],
+            expected: entry[2]
+        });
+    }
+
+    return result;
+}
+
+const positiveTestsParameters = parseCSVContent(readCSVFile(POSITIVE_TESTS_CSV_PATH));
+const negativeTestsParameters = parseCSVContent(readCSVFile(NEGATIVE_TESTS_CSV_PATH));
+
+describe("Positive Pricing2Yaml Parser Tests", () => {
+    for (const {sectionName, tests} of positiveTestsParameters){
+        describe(sectionName, () => {
+            for (const {pricingPath, expected} of tests) {
+
+                const tempPricingPath = TEMP_FILE_PATH + pricingPath.split("/").pop();
+
+                before(() => {
+                    // Create a temp file from the TEST_PRICING_YAML_PATH file
+                    Deno.copyFileSync(pricingPath, tempPricingPath);
+                });
+            
+                it(`${expected} parsing`, () => {
+                    const pricing: Pricing = retrievePricingFromYaml(tempPricingPath);
+    
+                    assertEquals(pricing.saasName, expected);
+                    assertEquals(pricing.version, LATEST_PRICING2YAML_VERSION);
+                    assert(pricing.createdAt instanceof Date);
+                });
         
-        assertEquals(pricing.saasName, TEST_PRICING_NAME);
-        assertEquals(pricing.version, LATEST_PRICING2YAML_VERSION);
-        assert(pricing.createdAt instanceof Date);
-    });
+                after(() => {
+                    // Remove the modified temp file
+                    Deno.removeSync(tempPricingPath);
+                });
+            }}
+        );
+    }
+});
 
-    // Hay que meter tests negativos
+describe("Negative Pricing2Yaml Parser Tests", () => {
+    for (const {sectionName, tests} of negativeTestsParameters) {
+        describe(sectionName, () => {
+            for (const {testName, pricingPath, expected} of tests) {
+
+                const tempPricingPath = TEMP_FILE_PATH + pricingPath.split("/").pop();
+
+                before(() => {
+                    // Create a temp file from the TEST_PRICING_YAML_PATH file
+                    Deno.copyFileSync(pricingPath, tempPricingPath);
+                });
+            
+                it(`${testName}`, () => {
+                    try {
+                        const _pricing: Pricing = retrievePricingFromYaml(tempPricingPath);
+                        assert(false, "Expected an error to be thrown");
+                    } catch (error) {
+                        assertIsError(error);
+                        assertEquals(error.message, expected);
+                    }
+                });
+    
+                after(() => {
+                    // Remove the modified temp file
+                    Deno.removeSync(tempPricingPath);
+                });
+            }
+        });
+    }
 });
