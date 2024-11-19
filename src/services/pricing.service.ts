7a2b0be80626c64db2be75dd970f783c6723ac77
Alex-GF <yalejandro9@gmail.com>
2024-11-19 22:28:41 +0100

    FIX: MINIZINC RUNNER

--- src/services/pricing.service.ts
+++ src/services/pricing.service.ts
@@ -5,9 +5,13 @@ import { PricingOperation } from '../models/minizinc/minizinc';
 import { ErrorMessage } from 'minizinc';
 
 export interface PricingAnalytics {
+  numberOfFeatures: number;
+  numberOfUsageLimits: number;
   numberOfPlans: number;
   numberOfAddOns: number;
   configurationSpaceSize: number;
+  minSubscriptionPrice: number;
+  maxSubscriptionPrice: number;
 }
 
 export default class PricingService {
@@ -21,22 +25,38 @@ export default class PricingService {
   async getAnalytics() {
 
     try{
-      const configurationSpaceSize = await this._getConfigurationSpaceSize();
+
+      const dznPricing = pricing2DZN(this.pricing);
+
+      const configurationSpaceSize = await this._getConfigurationSpaceSize(dznPricing);
+      const minSubscriptionPrice = await this._getMinSubscriptionPrice(dznPricing);
+      const maxSubscriptionPrice = await this._getMaxSubscriptionPrice(dznPricing);
       return {
+        numberOfFeatures: this.pricing.features.length,
+        numberOfUsageLimits: this.pricing.usageLimits ? this.pricing.usageLimits.length : 0,
         numberOfPlans: this.pricing.plans.length,
         numberOfAddOns: this.pricing.addOns ? this.pricing.addOns.length : 0,
         configurationSpaceSize: configurationSpaceSize.statistics.nSolutions,
+        minSubscriptionPrice: minSubscriptionPrice,
+        maxSubscriptionPrice: maxSubscriptionPrice,
       };
     }catch(e){
-      console.log(e);
       throw new Error((e as ErrorMessage).message);
     }
   }
   
-  async _getConfigurationSpaceSize() {
-    const dznPricing = pricing2DZN(this.pricing);
-
+  async _getConfigurationSpaceSize(dznPricing: string) {
     const model = new PricingCSP();
     return model.runPricingOperation(PricingOperation.CONFIGURATION_SPACE, dznPricing);
+  }
+
+  async _getMinSubscriptionPrice(dznPricing: string) {
+    const model = new PricingCSP();
+    return model.runPricingOperation(PricingOperation.CHEAPEST_SUBSCRIPTION, dznPricing);
+  }
+
+  async _getMaxSubscriptionPrice(dznPricing: string) {
+    const model = new PricingCSP();
+    return model.runPricingOperation(PricingOperation.MOST_EXPENSIVE_SUBSCRIPTION, dznPricing);
   }
 }
