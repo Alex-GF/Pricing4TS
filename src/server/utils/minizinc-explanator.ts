ab9f064100e2e37fae98c4df721c5d680fea14d0
Alex-GF <yalejandro9@gmail.com>
2025-03-28 18:58:15 +0100

    feat: added validation to configuration space operation to find dead usage limtis

--- src/server/utils/minizinc-explanator.ts
+++ src/server/utils/minizinc-explanator.ts
@@ -1,3 +1,4 @@
+import { ValueType } from '../../main/models/pricing2yaml/types';
 import { AddOn, Plan, Pricing } from '../../types';
 import { calculateOverriddenValue } from './dzn-exporter/number-utils';
 
@@ -9,6 +10,8 @@ export function explain(minizincError: string, pricing: Pricing): string {
       return explainUsageLimitWithoutValueError(pricing, errorMessage);
     case 'DeadFeatureError':
       return explainDeadFeatureError(pricing, errorMessage);
+    case 'DeadUsageLimitError':
+      return explainDeadUsageLimitError(pricing, errorMessage);
     default:
       return minizincError;
   }
@@ -55,10 +58,47 @@ function explainDeadFeatureError(pricing: Pricing, errorMessage: string): string
   return `${errorMessage} Found dead features: ` + deadFeatures.join(', ');
 }
 
+function explainDeadUsageLimitError(pricing: Pricing, errorMessage: string): string {
+  const pricingUsageLimits = Object.keys(pricing.usageLimits!);
+  const deadUsageLimits = [];
+
+  for (const usageLimit of pricingUsageLimits) {
+    let isUsageLimitDead = !(
+      _isUsageLimitInAnyPlan(usageLimit, pricing.plans) ||
+      _isUsageLimitInAnyAddOn(
+        usageLimit,
+        pricing.usageLimits![usageLimit].valueType,
+        pricing.addOns
+      )
+    );
+
+    if (isUsageLimitDead) {
+      deadUsageLimits.push(usageLimit);
+    }
+  }
+
+  return `${errorMessage} Found dead usage limits: ` + deadUsageLimits.join(', ');
+}
+
 function _isFeatureInAnyPlan(feature: string, plans: Record<string, Plan> | undefined): boolean {
   if (plans) {
     for (const plan of Object.values(plans)) {
       if (plan.features[feature].value || plan.features[feature].defaultValue) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function _isUsageLimitInAnyPlan(
+  usageLimit: string,
+  plans: Record<string, Plan> | undefined
+): boolean {
+  if (plans) {
+    for (const plan of Object.values(plans)) {
+      if (calculateOverriddenValue(plan.usageLimits![usageLimit]) > 0) {
         return true;
       }
     }
@@ -79,13 +119,40 @@ function _isFeatureInAnyAddOn(feature: string, addOns: Record<string, AddOn> | u
   return false;
 }
 
+function _isUsageLimitInAnyAddOn(
+  usageLimit: string,
+  usageLimitValueType: ValueType,
+  addOns: Record<string, AddOn> | undefined
+): boolean {
+  if (addOns) {
+    for (const addOn of Object.values(addOns)) {
+      // If the usage limit has a value in the add-on, or extend the plan's value, it is considered active
+      if (
+        (addOn.usageLimits?.[usageLimit] &&
+          calculateOverriddenValue({
+            ...addOn.usageLimits?.[usageLimit]!,
+            valueType: usageLimitValueType,
+          }) > 0) ||
+        (addOn.usageLimitsExtensions?.[usageLimit] &&
+          calculateOverriddenValue({
+            ...addOn.usageLimitsExtensions?.[usageLimit]!,
+            valueType: usageLimitValueType,
+          }) > 0)
+      ) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
 function _plansWithInactiveUsageLimit(
   usageLimit: string,
   linkedFeatures: string[] | undefined,
   pricing: Pricing,
   featuresLinkedToManyUsageLimits: string[]
 ): string[] {
-
   const plansWithInactiveUsageLimit: string[] = [];
 
   if (pricing.plans && linkedFeatures && linkedFeatures.length > 0) {
@@ -96,13 +163,22 @@ function _plansWithInactiveUsageLimit(
       // If no feature linked to the limit is active in the plan, an inactive usage limit is not invalid, so there is no need to perform the evaluation
       for (const feature of Object.values(planFeatures)) {
         if (linkedFeatures.includes(feature.name)) {
-          if (!featuresLinkedToManyUsageLimits.includes(feature.name) && (feature.value || feature.defaultValue)) {
+          if (
+            !featuresLinkedToManyUsageLimits.includes(feature.name) &&
+            (feature.value || feature.defaultValue)
+          ) {
             isPlanWithLinkedFeatureActive = true;
             break;
-          }else if(featuresLinkedToManyUsageLimits.includes(feature.name)) {
-            const usageLimitsLinkedToFeature = Object.values(pricing.usageLimits!).map(u => u.linkedFeatures?.includes(feature.name) );
+          } else if (featuresLinkedToManyUsageLimits.includes(feature.name)) {
+            const usageLimitsLinkedToFeature = Object.values(pricing.usageLimits!).map(u =>
+              u.linkedFeatures?.includes(feature.name)
+            );
 
-            if (usageLimitsLinkedToFeature.some(u => calculateOverriddenValue(plan.usageLimits![usageLimit]) > 0)){
+            if (
+              usageLimitsLinkedToFeature.some(
+                u => calculateOverriddenValue(plan.usageLimits![usageLimit]) > 0
+              )
+            ) {
               isPlanWithLinkedFeatureActive = true;
               break;
             }
@@ -125,7 +201,7 @@ function _plansWithInactiveUsageLimit(
   return plansWithInactiveUsageLimit;
 }
 
-function _findFeaturesLinkedToManyUsageLimits(pricing: Pricing){
+function _findFeaturesLinkedToManyUsageLimits(pricing: Pricing) {
   const pricingUsageLimits = pricing.usageLimits!;
 
   const linkedFeaturesCounter: Record<string, number> = {};
@@ -143,7 +219,7 @@ function _findFeaturesLinkedToManyUsageLimits(pricing: Pricing){
     }
   }
 
-  console.log(linkedFeaturesCounter)
+  console.log(linkedFeaturesCounter);
 
   return Object.entries(linkedFeaturesCounter)
     .filter(([_, count]) => count > 1)
